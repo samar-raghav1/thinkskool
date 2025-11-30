349c26844e86b28fc3f36323aa4338151857298e
samar-raghav1 <samarraghav001@gmail.com>
2025-11-30 10:40:22 +0530

    commited contact

--- Client/src/components/Contact.jsx
+++ Client/src/components/Contact.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { FiMail, FiPhone, FiCalendar, FiX, FiArrowLeft } from 'react-icons/fi';
